b76bb42cf418f337e3a8261edb80659c68a4dbe4
Yofti Milkessa <yoftahemilkessa@gmail.com>
2023-01-28 02:56:16 -0500

    Reformat the key storing functionality. Now only saves keys if explicitly requested after creation

--- key_pair.py
+++ key_pair.py
@@ -16,7 +16,7 @@ class KeyPair:
     for creating and loading key pairs.
     """
 
-    def __init__(self, private_key, key_store_file) -> None:
+    def __init__(self, private_key, key_store_file=None) -> None:
         self.private_key = private_key
         self.key_store_file = key_store_file
 
@@ -38,6 +38,37 @@ class KeyPair:
     def delete_key_file(self):
         os.remove(self.key_store_file)
     
+    def sign(self, content):
+        """
+        Returns the singature for the content
+        """
+        return self.priv_key.schnorr_sign(content)
+
+    def verify_signature(self, content, signature):
+        return self.private_key.schnorr_verify(content, signature)
+    
+    def save_key(self, file_name=KEY_STORAGE_FILE, set_key_store=True):
+        """
+        Encrypts and saves the private key to a file.
+        """
+        # Get a password with which to encrypt and save the private key
+        password = "x"
+        re_entered = ""
+        while password != re_entered or not password:
+            password = input("Enter password below:\n")
+            re_entered = input("Confirm password below:\n")
+        hash_str = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        hash_as_bytes = bytes.fromhex(hash_str)
+        b64_version = b64encode(hash_as_bytes)
+        f = Fernet(b64_version)
+
+        # Now encrypt and save the key
+        encrypted_key = f.encrypt(self.private_key_bytes())
+        with open(file_name, 'wb') as storage_file:
+            storage_file.write(encrypted_key)
+        if set_key_store:
+            self.key_store_file = file_name
+
     @staticmethod
     def load_key_pair(file_name = KEY_STORAGE_FILE):
         """
@@ -56,7 +87,7 @@ class KeyPair:
         return KeyPair(priv_key, file_name)
 
     @staticmethod
-    def create_new_key_pair(file_name = KEY_STORAGE_FILE):
+    def create_new_key_pair():
         """
         Creates a new valid key pair using the secp256k1 curve and saves it to a file.
         Essentially creates the keys for a new Nostr user.
@@ -64,20 +95,4 @@ class KeyPair:
         secret_bytes = secrets.token_bytes(32)
         priv_key = secp256k1.PrivateKey(secret_bytes, raw=True)
 
-        # Get a password with which to encrypt and save the private key
-        password = "x"
-        re_entered = ""
-        while password != re_entered or not password:
-            password = input("Enter password below:\n")
-            re_entered = input("Confirm password below:\n")
-        hash_str = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        hash_as_bytes = bytes.fromhex(hash_str)
-        b64_version = b64encode(hash_as_bytes)
-        f = Fernet(b64_version)
-
-        # Now encrypt and save the key
-        encrypted_key = f.encrypt(secret_bytes)
-        with open(file_name, 'wb') as storage_file:
-            storage_file.write(encrypted_key)
-
-        return KeyPair(priv_key, file_name)
+        return KeyPair(priv_key)
--- test.py
+++ test.py
@@ -9,8 +9,9 @@ class KeyPairTests(unittest.TestCase):
     def setUp(self) -> None:
         self.key_store_file = "mykeys_test"
         self.password = "test123"
+        key_pair = KeyPair.create_new_key_pair()
         with patch('builtins.input', return_value=self.password):
-            key_pair = KeyPair.create_new_key_pair(self.key_store_file)
+            key_pair.save_key(self.key_store_file)
 
     def tearDown(self) -> None:
         with patch('builtins.input', return_value=self.password):
@@ -20,22 +21,22 @@ class KeyPairTests(unittest.TestCase):
 
     def test_key_loading(self):
         test_file_name = "mykeys_test_loading"
+        key_pair = KeyPair.create_new_key_pair()
         with patch('builtins.input', return_value=self.password):
-            key_pair = KeyPair.create_new_key_pair(test_file_name)
+            key_pair.save_key(test_file_name)
         private_key_str = key_pair.hex_priv_key()
         public_key_str = key_pair.hex_pub_key()
 
         # Now load the keys from file
         with patch('builtins.input', return_value=self.password):
             loaded_keys = KeyPair.load_key_pair(test_file_name)
-        loaded_private_key = key_pair.hex_priv_key()
+        loaded_private_key = loaded_keys.hex_priv_key()
         loaded_public_key = loaded_keys.hex_pub_key()
         
         # Ensure that the created keys are the same as the loaded keys.
         self.assertEqual(private_key_str, loaded_private_key)
         self.assertEqual(public_key_str, loaded_public_key)
         key_pair.delete_key_file()
-
 
 if __name__ == '__main__':
     unittest.main()
